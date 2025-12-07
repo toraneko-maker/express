01fe8339cf20b9010a1bf0c8fcfb251a004424e4
山本由紀 <yamamotoyuki@yamamotoyukinoMacBook-Air.local>
2025-12-07 17:37:12 +0900

    Disable MongoDB in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+// MongoDB を使わないのでコメントアウト
 // 接続情報を設定
-const { MongoClient } = require("mongodb");
-const uri = "*************";
-const client = new MongoClient(uri);
+//const { MongoClient } = require("mongodb");
+//const uri = "*************";
+//const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
